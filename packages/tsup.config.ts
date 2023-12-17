5a9c5ccd2d1ef580cea1d940afaf0ef091e192f9
ntnyq <ntnyq13@gmail.com>
2023-12-17 21:30:51 +0800

    chore: bump deps

--- packages/tsup.config.ts
+++ packages/tsup.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['./src/*.ts'],
   format: ['cjs', 'esm'],
-  target: 'node16',
+  target: ['node18', 'es2022'],
   clean: true,
   shims: true,
   cjsInterop: true,

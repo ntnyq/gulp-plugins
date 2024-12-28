31bd0bf7aa1064a09c6866a287f79158ae87f523
ntnyq <ntnyq13@gmail.com>
2024-12-29 00:46:40 +0800

    chore: update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,4 @@
-/**
- * @file ESLint config
- *
- * @see {@link https://eslint.org/docs/user-guide/configuring}
- */
+// @ts-check
 
 import { defineESLintConfig } from '@ntnyq/eslint-config'
 
--- packages/tsup.config.ts
+++ packages/tsup.config.ts
@@ -3,16 +3,16 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   cjsInterop: true,
   clean: true,
-  dts: {
-    compilerOptions: {
-      composite: false,
-      paths: {},
-    },
-  },
   entry: ['./src/*.ts'],
   format: ['cjs', 'esm'],
   shims: true,
   splitting: true,
   target: ['node18', 'es2022'],
   tsconfig: '../../tsconfig.lib.json',
+  dts: {
+    compilerOptions: {
+      composite: false,
+      paths: {},
+    },
+  },
 })
